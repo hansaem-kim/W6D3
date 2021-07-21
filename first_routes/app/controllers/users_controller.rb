324cf5bc308d747a8693cb15a1a16ef9d01d9eaa
Hansaem Kim <hansaem_kim@hotmail.com>
2021-07-21 14:40:42 -0400

    finished plyaing with params

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -3,4 +3,13 @@ class UsersController < ApplicationController
     def index 
         render plain: 'Hello world'
     end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
+
 end
